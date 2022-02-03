6aebc7772b31e99b6c10744506735e657c714e20
abd <abd.manian@gmail.com>
2022-02-03 12:01:12 +0330

    Delete code

--- ask_age/src/ask_age.cpp
+++ ask_age/src/ask_age.cpp
@@ -2,15 +2,4 @@
 
 int main() {
 
-	int age {0};
-
-	do
-	{
-		std::cout << "Please enter your age (enter negative for exit):";
-		std::cin >> age;
-		std::cout << "You had " << age * 365 << " great days!\n";
-
-	} while(age>0);
-
-	return 0;
 }
