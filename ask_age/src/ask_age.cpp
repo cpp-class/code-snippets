12e9cb9eb100c357fa88d7635560c3c82ae7a97e
abd <abd.manian@gmail.com>
2022-02-03 11:20:25 +0330

    Add a look for ask age

--- ask_age/src/ask_age.cpp
+++ ask_age/src/ask_age.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-	std::cout << "!!!Hello World!!!\n";
+
+	int age {0};
+
+	do
+	{
+		std::cout << "Please enter your age (enter negative for exit):";
+		std::cin >> age;
+		std::cout << "You had " << age * 365 << " great days!\n";
+
+	} while(age>0);
+
 	return 0;
 }
