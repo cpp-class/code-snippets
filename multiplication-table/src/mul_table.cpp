477e2cb1e1f4fa3aa56f201bb455614188fd0945
abd <abd.manian@gmail.com>
2022-02-04 23:49:29 +0330

    refactoring mul-table

--- multiplication-table/src/mul_table.cpp
+++ multiplication-table/src/mul_table.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 
-
-void print_multiplication_table(int width, int height)
+int get_num_digits(int value)
 {
 	int num_digits {0};
-	int max_value {width * height};
-	int max_digits {0};
 
-	if (max_value<10) {
-		max_digits = 1;
-	} else if (max_value<100) {
-		max_digits = 2;
-	} else if (max_value<1000) {
-		max_digits = 3;
-	} else {
-		max_digits = 4;
+	while (value!=0)
+	{
+		num_digits += 1;
+		value = value / 10;
 	}
 
+	return num_digits;
+}
 
+void print_spaces(int num_spaces)
+{
+	for (int space = 0; space < num_spaces; ++space) {
+		std::cout << " ";
+	}
+
+}
+
+void print_multiplication_table
+		(int width, int height, int num_padding=1, bool right_align=false)
+{
+	int max_value {width * height};
+	int max_digits {get_num_digits(max_value)};
+	const int padding {1};
 
 	for (int y = 1; y <= height; ++y)
 	{
@@ -25,23 +34,13 @@ void print_multiplication_table(int width, int height)
 		{
 			int mul {x*y};
 
-			if (mul<10) {
-				num_digits = 1;
-			} else if (mul<100) {
-				num_digits = 2;
-			} else if (mul<1000) {
-				num_digits = 3;
-			} else {
-				num_digits = 4;
-			}
+			int num_digits {get_num_digits(mul)};
 
-			int required_space {max_digits - num_digits + 1};
+			int required_space {max_digits - num_digits + padding};
 
 			std::cout << mul;
 
-			for (int space = 0; space < required_space; ++space) {
-				std::cout << " ";
-			}
+			print_spaces(required_space);
 
 		}
 		std::cout << "\n";
